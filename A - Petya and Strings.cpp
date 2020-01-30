d58c64310b3228f8d9ac6a9036fc70ae4b9e48c0
Paulo Emil <42085335+pauloemil@users.noreply.github.com>
2020-01-30 10:18:40 +0200

    Create A - Petya and Strings.cpp

---
+++ A - Petya and Strings.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    for (int i = 0 ; i < a.size(); i++)
+    {
+        a[i] = tolower(a[i]);
+        b[i] =  tolower(b[i]);
+    }
+    if (a < b)
+        cout << -1;
+    else if (b < a)
+        cout << 1;
+    else
+        cout << 0;
+}
